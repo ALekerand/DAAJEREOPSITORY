ad120acaba43947c8a2319de02d4fcb1b4b4e5cf
gbane <gbane@GM-DTSI-MENA>
2023-09-13 12:36:52 +0000

    correction page niveau animateur

--- sygca/src/main/java/com/daaje/controllers/NiveauAnimateurControllers.java
+++ sygca/src/main/java/com/daaje/controllers/NiveauAnimateurControllers.java
@@ -3,9 +3,11 @@ package com.daaje.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.component.commandbutton.CommandButton;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,25 +21,47 @@ public class NiveauAnimateurControllers {
 	public NiveauAnimateur niveauAnimateur = new NiveauAnimateur();
 	public NiveauAnimateur selectedObject = new NiveauAnimateur();
 	public List listObject = new ArrayList();
+	
+//Controle des composants
+	public CommandButton cmdBModifier = new CommandButton();
+	public CommandButton cmdBEnregistrer = new CommandButton();
 		
 //Methodes
+	@PostConstruct
+	public void initialisation(){
+		cmdBModifier.setDisabled(true);
+	}
+	
+	
 	public void enregistrer(){
 		iservice.addObject(niveauAnimateur);
 		annuler();
 		info("Enregistrement effectué");
 	}
 	
+	public void modifier() {
+		System.out.println("===============information:"+niveauAnimateur.getCodeNiveau());
+		iservice.updateObject(niveauAnimateur);
+		annuler();
+		info("Modification effectuée");
+	}
+	
 	public void annuler() {
 		niveauAnimateur.setCodeNiveau(null);
 		niveauAnimateur.setNomNiveau(null);
+		cmdBEnregistrer.setDisabled(false);
+		cmdBModifier.setDisabled(true);
+		
 	}
 	
 	public void selectionnerLigne() {
 		niveauAnimateur = selectedObject;
+		cmdBEnregistrer.setDisabled(true);
+		cmdBModifier.setDisabled(false);
 	}
 	
 	public void info(String message){
-	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", message));	
+	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,message,null));	
 	}
 		
 //Getters and setters
@@ -63,6 +87,22 @@ public class NiveauAnimateurControllers {
 
 	public void setSelectedObject(NiveauAnimateur selectedObject) {
 		this.selectedObject = selectedObject;
+	}
+
+	public CommandButton getCmdBModifier() {
+		return cmdBModifier;
+	}
+
+	public void setCmdBModifier(CommandButton cmdBModifier) {
+		this.cmdBModifier = cmdBModifier;
+	}
+
+	public CommandButton getCmdBEnregistrer() {
+		return cmdBEnregistrer;
+	}
+
+	public void setCmdBEnregistrer(CommandButton cmdBEnregistrer) {
+		this.cmdBEnregistrer = cmdBEnregistrer;
 	}
 
 }
